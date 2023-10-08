38b453277dc96ad3d5b8786d055b29f7b2129c34
Tim Schindler <tim@iosharp.com>
2023-10-08 19:34:21 +0200

    initial, working release

---
+++ CyberArk.PSM.GenerateTOTPPreconnect/GenerateTOTP.cs
@@ -0,0 +1,50 @@
+﻿using CyberArk.PSM.WebAppDispatcher.PreconnectUtils;
+using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security;
+using System.Text;
+
+namespace CyberArk.PSM.GenerateTOTPPreconnect
+{   
+    
+    public class GenerateTOTP : IPreconnectContract
+    {
+        private readonly string TOTP_OUTPUT_PARAMETER = "MFACode";
+
+        public Dictionary<string, SecureString> GetParameters(Dictionary<string, SecureString> parameters, LogUtils.WriteToLogHandler writeToLogMethod)
+        {
+            string totpCode;
+
+            try
+            {
+                byte[] totpSecretBytes = Encoding.UTF8.GetBytes(new NetworkCredential(string.Empty, parameters["logonaccount_password"]).Password);
+
+                writeToLogMethod("Generating TOTP.", Consts.LOG_LEVEL_INFO);
+                
+                var totp = new Totp(totpSecretBytes);
+                totpCode = totp.ComputeTotp();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                writeToLogMethod(string.Format("Key not found in parameters dictionary: {0}", ex.ToString()), Consts.LOG_LEVEL_ERROR);
+                throw new PreconnectException("Key not found in parameters dictionary");
+            }
+            catch (Exception ex)
+            {
+                writeToLogMethod(string.Format("Error while generating TOTP: {0}", ex.ToString()), Consts.LOG_LEVEL_ERROR);
+                throw new PreconnectException("Error while generating TOTP");
+            }
+
+            writeToLogMethod("Successfully generated TOTP.", Consts.LOG_LEVEL_INFO);
+
+            return new Dictionary<string, SecureString> {
+                {
+                    TOTP_OUTPUT_PARAMETER,
+                    new NetworkCredential(string.Empty, totpCode).SecurePassword
+                }
+            };
+        }
+    }
+}
---
+++ CyberArk.PSM.GenerateTOTPPreconnect/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CyberArk.PSM.GenerateTOTPPreconnect")]
+[assembly: AssemblyDescription("A Preconnect DLL to generate a TOTP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tim Schindler")]
+[assembly: AssemblyProduct("CyberArk.PSM.GenerateTOTPPreconnect")]
+[assembly: AssemblyCopyright("Copyright © Tim Schindler 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5879f87d-4c16-4f7a-b616-6f9f44dde011")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
